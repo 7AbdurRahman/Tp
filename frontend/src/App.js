7298f8f5a26beec415315a637c3f751a9cc3d1b4
7AbdurRahman <ar10883338@gmail.com>
2024-03-28 08:43:38 +0530

    second frontend commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Login from "./login/Login.js";
+import Register from "./login/Register.js";
+import "./App.css";
 
 const App = () => {
   return (
-    <>
-      <div>
-        <h1>hello</h1>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 };
 
